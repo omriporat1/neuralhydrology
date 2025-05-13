5a533ee3df723b5c64f5cec044e0433b5eb85403
Omri Porat <152407960+omriporat1@users.noreply.github.com>
2025-05-13 15:20:30 +0300

    commit after creating short time run file for cpu (all else similar)

---
+++ Experiments/expand_stations_and_periods/new_configuration_wide_data_with_static/run_tutorial_cuda2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=gpu_job_short_trycpu
+#SBATCH --time=1:00:00
+
+#SBATCH --mem=32G
+#SBATCH --output=cuda_omri_job_output_short_trycpu.log
+#SBATCH --error=cuda_omri_job_error_short_trycpu.log
+#SBATCH --account=efratmorin
+
+
+module load spack miniconda3 cuda/11.7
+
+source /usr/local/spack/opt/spack/linux-debian12-x86_64/gcc-12.2.0/miniconda3-24.3.0-iqeknetqo7ngpr57d6gmu3dg4rzlcgk6/etc/profile.d/conda.sh
+
+conda activate /sci/labs/efratmorin/haimasree/condaenvs/neuralhydrology
+
+python /sci/labs/efratmorin/omripo/PhD/Python/neuralhydrology/Experiments/expand_stations_and_periods/new_configuration_wide_data_with_static/Best_HPC_omri_trial_cluster_trycpu.py
+
+conda deactivate
+
+
