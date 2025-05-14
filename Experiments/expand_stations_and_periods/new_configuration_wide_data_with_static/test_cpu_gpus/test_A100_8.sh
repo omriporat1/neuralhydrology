08265cd30ff301a78f434dab10064be4b1f8739c
Omri Porat <152407960+omriporat1@users.noreply.github.com>
2025-05-14 10:57:00 +0300

    Created config files to test 1 year of training, validation and test over the cpu and A30, A100 GPUs in the cluster

---
+++ Experiments/expand_stations_and_periods/new_configuration_wide_data_with_static/test_cpu_gpus/test_A100_8.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=test_runtime_A100_8
+#SBATCH --gres=gpu:a100:1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=1:30:00
+#SBATCH --mem=32G
+#SBATCH --output=test_runtime_A100_8_job_output.log
+#SBATCH --error=test_runtime_A100_8_error.log
+#SBATCH --account=efratmorin
+
+
+module load spack miniconda3 cuda/11.7
+
+source /usr/local/spack/opt/spack/linux-debian12-x86_64/gcc-12.2.0/miniconda3-24.3.0-iqeknetqo7ngpr57d6gmu3dg4rzlcgk6/etc/profile.d/conda.sh
+
+conda activate /sci/labs/efratmorin/haimasree/condaenvs/neuralhydrology
+
+nvidia-smi
+
+start=$(date +%s)
+
+python /sci/labs/efratmorin/omripo/PhD/Python/neuralhydrology/Experiments/expand_stations_and_periods/new_configuration_wide_data_with_static/test_cpu_gpus/test_A100_8.py
+
+end=$(date +%s)
+
+runtime=$((end-start))
+
+printf 'Training time: %02d:%02d:%02d\n' $((duration/3600)) $((duration%3600/60)) $((duration%60))
+
+conda deactivate
+
+
