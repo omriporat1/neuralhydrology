0a6a4c7996c6d4e6cbe68bc84a54f39422bfb23a
Omri Porat <152407960+omriporat1@users.noreply.github.com>
2025-06-04 21:26:19 +0300

    visualization of hyperparameter space

--- Experiments/HPC_random_search/generate_config_csv.py
+++ Experiments/HPC_random_search/generate_config_csv.py
@@ -4,6 +4,7 @@ import numpy as np
 
 N = 60  # Number of random configs
 
+'''
 param_space = {
     'batch_size': [256, 512, 1024],
     'hidden_size': [128, 256, 512],
@@ -12,10 +13,22 @@ param_space = {
     'seq_length': [36, 72, 144],
     'statics_embedding': [5, 10, 15, 20]
 }
+'''
 
+
+param_space = {
+    'batch_size': [128, 256, 512, 1024, 2048],
+    'hidden_size': [32, 64, 128, 256, 512, 1024],
+    'learning_rate': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1],
+    'output_dropout': [0, 0.2, 0.4, 0.6, 0.8],
+    'seq_length': [36, 72, 144, 216, 288],
+    'statics_embedding': [5, 10, 15, 20, 25]
+}
+
+# Generate random samples from the parameter space
 samples = pd.DataFrame({
     k: np.random.choice(v, size=N)
     for k, v in param_space.items()
 })
 
-samples.to_csv("random_search_configurations.csv", index_label="job_id")
+samples.to_csv("random_search_configurations_denser.csv", index_label="job_id")
