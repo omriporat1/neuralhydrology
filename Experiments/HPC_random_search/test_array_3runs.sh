bd194fefe96bcb6c7ba11fac6e45ca92d8ba5385
Omri Porat <152407960+omriporat1@users.noreply.github.com>
2025-08-03 12:54:19 +0300

    Update SLURM job resource allocation and correct conda source path

--- Experiments/HPC_random_search/test_array_3runs.sh
+++ Experiments/HPC_random_search/test_array_3runs.sh
@@ -4,15 +4,14 @@
 #SBATCH --array=0-2
 #SBATCH --gres=gpu:a100:1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=32G
-#SBATCH --time=4:00:00
+#SBATCH --mem=64G
+#SBATCH --time=12:00:00
 #SBATCH --output=logs/test_output_%A_%a.log
 #SBATCH --error=logs/test_error_%A_%a.log
 #SBATCH --account=efratmorin
 
 module load spack miniconda3 cuda/11.7 nvidia
-
-source /usr/local/spack/opt/spack/linux-debian12-x86_64/gcc-12.2.0/iqeknetqo7ngpr57d6gmu3dg4rzlcgk6/etc/profile.d/conda.sh
+source /usr/local/spack/opt/spack/linux-debian12-x86_64/gcc-12.2.0/miniconda3-24.3.0-iqeknetqo7ngpr57d6gmu3dg4rzlcgk6/etc/profile.d/conda.sh
 
 conda activate /sci/labs/efratmorin/omripo/condaenvs/neuralhydrology
 
