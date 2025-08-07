3f51ac179696782da13fa322ddfb0f507f0731a6
Omri Porat <152407960+omriporat1@users.noreply.github.com>
2025-08-07 10:18:25 +0300

    Enhance IDW interpolation by adding progress logging for chunk processing and refactoring argument handling for clarity and maintainability.

---
+++ Experiments/Calculate_mean_basin_rain_from_gauges/calculate_basin_average_rain/src/calculate_idw_surface_cluster_with_prints.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=idw_surface
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=400G
+#SBATCH --time=2-00:00:00
+#SBATCH --output=idw_surface_%j.log
+#SBATCH --error=idw_surface_error_%j.log
+#SBATCH --account=efratmorin
+
+
+module load spack miniconda3
+
+source /usr/local/spack/opt/spack/linux-debian12-x86_64/gcc-12.2.0/miniconda3-24.3.0-iqeknetqo7ngpr57d6gmu3dg4rzlcgk6/etc/profile.d/conda.sh
+
+conda activate /sci/labs/efratmorin/omripo/condaenvs/neuralhydrology
+
+mkdir -p logs
+
+/usr/bin/time -v python calculate_idw_surface_cluster_parallel_with_prints.py
+
+conda deactivate
