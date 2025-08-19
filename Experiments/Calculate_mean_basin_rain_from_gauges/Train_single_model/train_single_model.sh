107113fc9c495345cc7a3fbdaaca693919b3fb46
Omri Porat <152407960+omriporat1@users.noreply.github.com>
2025-08-19 17:20:03 +0300

    Update logging filenames in training script: include job name and ID in output and error log filenames.

--- Experiments/Calculate_mean_basin_rain_from_gauges/Train_single_model/train_single_model.sh
+++ Experiments/Calculate_mean_basin_rain_from_gauges/Train_single_model/train_single_model.sh
@@ -5,11 +5,11 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=256G
 #SBATCH --time=36:00:00
-#SBATCH --output=logs/output_avg_rain.log
-#SBATCH --error=logs/error_avg_rain.log
+#SBATCH --output=logs/output-%x-%j.log
+#SBATCH --error=logs/error-%x-%j.log
 #SBATCH --account=efratmorin
 
-module load spack miniconda3 cuda/11.7
+module load spack miniconda3 cuda/11.7 nvidia
 
 source /usr/local/spack/opt/spack/linux-debian12-x86_64/gcc-12.2.0/miniconda3-24.3.0-iqeknetqo7ngpr57d6gmu3dg4rzlcgk6/etc/profile.d/conda.sh
 
@@ -21,7 +21,7 @@ nvidia-smi
 
 start=$(date +%s)
 
-nvidia-smi --query-gpu=timestamp,index,name,utilization.gpu,utilization.memory,memory.used --format=csv -l 60 > logs/gpu_usage_${SLURM_JOB_ID}.log &
+nvidia-smi --query-gpu=timestamp,index,name,utilization.gpu,utilization.memory,memory.used --format=csv -l 60 > "logs/gpu_usage_${SLURM_JOB_ID}.log" &
 GPU_MON_PID=$!
 
 /usr/bin/time -v python train_single_model_with_av_rain.py
